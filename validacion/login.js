ade507490d637db4e2466a520c757aea95e2b8e0
jjohan2001 <johan.maldonado72@misena.edu.co>
2022-05-24 16:34:12 -0500

    .

--- validacion/login.js
+++ validacion/login.js
@@ -1,5 +1,18 @@
 function validacion(){
 
-    var usuario = 
+    var usuario = document.getElementById("usuario").value;
+    var password = document.getElementById("password").value;
+    var rol = document.getElementById("rol").value;
+
+    if (usuario == ""){
+        alert ("porfavor ingresar su nombre")
+    }
+        if (password == ""){
+            alert("porfavor infgrese su contraseña")
+        }
+        if (rol == ""){
+            alert ("porfavor ingrsar un rol")
+        }
+
 
 }
