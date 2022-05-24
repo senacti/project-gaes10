43712adac4ba23302cc5e353c237d259128c5fe1
jjohan2001 <johan.maldonado72@misena.edu.co>
2022-05-24 17:49:39 -0500

    validacion

--- validacion/login.js
+++ validacion/login.js
@@ -1,18 +1,35 @@
+var attempt = 3; 
 function validacion(){
 
     var usuario = document.getElementById("usuario").value;
     var password = document.getElementById("password").value;
     var rol = document.getElementById("rol").value;
 
-    if (usuario == ""){
-        alert ("porfavor ingresar su nombre")
+    if (usuario ==""){
+        alert ("Ingrese su nombre")
     }
-        if (password == ""){
-            alert("porfavor infgrese su contraseña")
+        if (password ==""){
+            alert("Ingrese su contraseña")
         }
-        if (rol == ""){
-            alert ("porfavor ingrsar un rol")
+        if(rol==""){
+            alert("ingrese un rol")
         }
 
-
+    if(usuario =="sebastian" && password == "123" && rol=="coordinador"){
+        alert("bienbenido");
+        window.location = "padmin.html" ;
+        return false;
+    }
+    else{
+        attempt--;
+        alert("Lo siento le quedan "+attempt+" intentos;");
+    
+        if(attempt==0){
+            document.getElementById("usuario").desabled = true;
+            document.getElementById("password").desabled = true;
+            document.getElementById("rol").desabled = true;
+            document.getElementById("buton").desabled = true;
+            return false;
+        }
+    }
 }
