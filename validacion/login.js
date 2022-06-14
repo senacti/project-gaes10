9cb9275029ac3fe24b682b3e891017a5218ee64c
jjohan2001 <johan.maldonado72@misena.edu.co>
2022-06-14 09:49:26 -0500

    .

--- validacion/login.js
+++ validacion/login.js
@@ -1,35 +1,29 @@
-var attempt = 3; 
-function validacion(){
+var attempt = 3;
+function validacion() {
+  var usuario = document.getElementById("usuario").value;
+  var password = document.getElementById("password").value;
+  var rol = document.getElementById("rol").value;
 
-    var usuario = document.getElementById("usuario").value;
-    var password = document.getElementById("password").value;
-    var rol = document.getElementById("rol").value;
+  if (username == "") alert("Por favor, ingresa un nombre");
 
-    if (usuario ==""){
-        alert ("Ingrese su nombre")
-    }
-        if (password ==""){
-            alert("Ingrese su contraseña")
-        }
-        if(rol==""){
-            alert("ingrese un rol")
-        }
+  if (password == "") alert("Por favor, ingresa un email");
 
-    if(usuario =="sebastian" && password == "123" && rol=="coordinador"){
-        alert("bienbenido");
-        window.location = "padmin.html" ;
-        return false;
+  if (rol == "") alert("Por favor, ingresa un rol");
+
+  if (usuario == "sebastian" && password == "123" && rol == "Coordinador") {
+    alert("INGRESO EXITOSO");
+    window.location = "admin.html"
+    return false;
+  } else {
+    attempt--;
+    alert("Lo siento le quedan " + attempt + " intentos;");
+
+    if (attempt == 0) {
+      document.getElementById("usuario").desabled = true;
+      document.getElementById("password").desabled = true;
+      document.getElementById("rol").desabled = true;
+      document.getElementById("buton").desabled = true;
+      return false;
     }
-    else{
-        attempt--;
-        alert("Lo siento le quedan "+attempt+" intentos;");
-    
-        if(attempt==0){
-            document.getElementById("usuario").desabled = true;
-            document.getElementById("password").desabled = true;
-            document.getElementById("rol").desabled = true;
-            document.getElementById("buton").desabled = true;
-            return false;
-        }
-    }
+  }
 }
