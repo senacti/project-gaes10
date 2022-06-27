864341197a8facda5a218ab3f4eca5a714503bc0
jjohan2001 <johan.maldonado72@misena.edu.co>
2022-06-27 14:09:52 -0500

    validacion full K4

--- fromulario.js
+++ fromulario.js
@@ -1,1 +1,19 @@
-var campo = $('#')
+function validate() {
+  const nombres = document.getElementById("nombres").value;
+  const apellidos = document.getElementById("apellidos").value;
+  const celular = document.getElementById("celular").value;
+  const cedula = document.getElementById("cedula").value;
+  const fecha = document.getElementById("fecha").value;
+  const servicio = document.getElementById("ciudad").value;
+
+
+
+  if (nombres == "" && apellidos == "" && celular == "" && cedula == "" && fecha == "" && servicio == "--Servicios Basicos--"){
+    alert("Ingrese todos los datos");
+
+  }else{
+    alert("formulario eviado")
+  }
+
+
+}
